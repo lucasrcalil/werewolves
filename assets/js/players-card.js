a2de9672553fbe96e4aee3b947f693436a4067cf
lucasrcalil <lucasrcalil@gmail.com>
2022-01-08 02:10:24 -0300

    Att

--- assets/js/players-card.js
+++ assets/js/players-card.js
@@ -1,6 +1,7 @@
 var blocks = document.querySelectorAll(".block-vert");
 var gameBlocks = document.querySelectorAll(".block");
 var homeBlocks = document.querySelectorAll(".home-block");
+var titleBlocks = document.querySelectorAll(".block-vert-titles");
 
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
@@ -25,6 +26,16 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
     homeBlocks.forEach((block, index) => {
+      setTimeout(() => {
+        block.classList.remove("inactive");
+      }, (index + 1) * 200);
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    titleBlocks.forEach((block, index) => {
       setTimeout(() => {
         block.classList.remove("inactive");
       }, (index + 1) * 200);
